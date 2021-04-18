0517f441950725c10526d09d2a49727384e5d408
Newton Toto <sokorototo@noreply.github.com>
2021-04-19 01:30:34 +0300

    Derp test

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,3 @@
-import Worley from "../build/worley.min.mjs";
+requirejs(["../build/worley.min.js"], function(Worley) {
 
-let canvas = document.getElementById("canvas")
-, ctx = canvas.getContext("2d")
-, options = {
-    width: 256,
-    height: 256,
-    seed: [12345, 45678, 67890, 242521],
-    colors: [[255, 134, 65], [123, 35, 100]],
-    interpolate: false,
-    threshold: 8
-}
-, button = document.getElementById("trigger")
-, inputs = Array.from(document.querySelectorAll("div.control > input"));
-button.onclick = () => {
-    let setup = {};
-    inputs.forEach(input => setup[input.name] = Number(input.value));
-    setup = Object.assign(options, setup);
-    let noise = new Worley(setup);
-    noise.Texture.ImageData().then((img) => {
-        console.log(`[IMG]: ${img}`);
-        ctx.putImageData(img, 0, 0);
-    }).catch(e => console.log(`%c [ERROR]: ${e.message}`, "color: red; font-family: monospace"));
-};
+});
