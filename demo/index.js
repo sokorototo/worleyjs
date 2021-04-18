2f9fdb7eff41dc394f82d0e312df7b0ef19c793d
Newton Toto <sokorototo@noreply.github.com>
2021-04-18 23:48:06 +0300

    Did too mush to reme,ber

--- demo/index.js
+++ demo/index.js
@@ -4,20 +4,19 @@ let canvas = document.getElementById("canvas")
     width: 256,
     height: 256,
     seed: [12345, 45678, 67890, 242521],
-    colors: [[255, 134, 65], [123, 35, 100]]
+    colors: [[255, 134, 65], [123, 35, 100]],
+    interpolate: false,
+    threshold: 8
 }
-, worker = new Worker("worker.js", {name: "worley_gen"})
 , button = document.getElementById("trigger")
 , inputs = Array.from(document.querySelectorAll("div.control > input"));
 button.onclick = () => {
     let setup = {};
     inputs.forEach(input => setup[input.name] = Number(input.value));
     setup = Object.assign(options, setup);
-    worker.postMessage(setup);
-};
-
-worker.onmessage = (evt) => {
-    let img = evt.data.data;
-    let data = new ImageData(img, evt.data.width, evt.data.height);
-    ctx.putImageData(data, 0, 0);
+    let noise = new Worley(setup);
+    noise.Texture.ImageData().then((img) => {
+        console.log(`[IMG]: ${img}`);
+        ctx.putImageData(img, 0, 0);
+    }).catch(e => console.log(`%c [ERROR]: ${e.message}`, "color: red; font-family: monospace"));
 };
