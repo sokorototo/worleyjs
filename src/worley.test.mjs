51fa7f2c6a3ec1c1b5a09a9211aaed1923d545ef
sokorototo <nyachiengatoto@gmail.com>
2024-05-31 13:53:31 +0300

    temporary work suspension

--- src/worley.test.mjs
+++ src/worley.test.mjs
@@ -3,74 +3,66 @@ import { createWriteStream } from "fs";
 import Worley from "../build/worley.min.mjs";
 
 // This test is built to run in a Node or Deno environment
-try{
-	const size = 512;
-	
-	let Noise = new Worley({
-		width: size,
-		height: size,
-		crests: 60,
-		threshold: size / 4,
-		colors: [[235, 201, 12], [44, 2, 2]],
-		alpha: false,
-		prerun: 5,
-		interpolate: true,
-		hierachy: 0,
-		interpolant: (min, max, x) => {
-			let noiseLevel = 0.35;
-			let y = Math.pow(Math.min(Math.cos(Math.PI * x / 2.0), 1.0 - Math.abs(x)), 3) + (noiseLevel * Math.random());
-			return (((max - min) * y) + min)
-		}
-	});
+const size = 512;
 
-	let then = Date.now();
+let Noise = new Worley({
+	width: size,
+	height: size,
+	crests: 60,
+	threshold: size / 4,
+	colors: [[235, 201, 12], [44, 2, 2]],
+	alpha: false,
+	prerun: 5,
+	interpolate: true,
+	hierachy: 0,
+	interpolant: (min, max, x) => {
+		let noiseLevel = 0.35;
+		let y = Math.pow(Math.min(Math.cos(Math.PI * x / 2.0), 1.0 - Math.abs(x)), 3) + (noiseLevel * Math.random());
+		return (((max - min) * y) + min)
+	}
+});
 
-	// Noise.pixel test
-	then = Date.now();
-	Noise.pixel(Noise.width / 2, Noise.height / 2, true, 1).then((output) => {
-		let now = Date.now();
-		console.log(`[OUTPUT]: Noise.pixel => ${output}`);
-		console.log(`[DEBUG]: Noise.pixel took ${(now - then) / 1000}s to execute`);
-		console.log(`[DEBUG]: Noise.pixel -> TEST PASSED\n`);
-	});
+let then = Date.now();
 
-	// Noise.nearestCrest test
-	then = Date.now();
-	Noise.nearestCrest(size, size, 0).then((output) => {
-		let now = Date.now();
-		console.log(`[OUTPUT]: Noise.nearestCrest => [X:${output[0]}, Y:${output[1]}]`);
-		console.log(`[DEBUG]: Noise.nearestCrest took ${(now - then) / 1000}s to execute`);
-		console.log(`[DEBUG]: Noise.nearestCrest -> TEST PASSED\n`);
+// Noise.pixel test
+then = Date.now();
+Noise.pixel(Noise.width / 2, Noise.height / 2, true, 1).then((output) => {
+	let now = Date.now();
+	console.log(`[OUTPUT]: Noise.pixel => ${output}`);
+	console.log(`[DEBUG]: Noise.pixel took ${(now - then) / 1000}s to execute`);
+	console.log(`[DEBUG]: Noise.pixel -> TEST PASSED\n`);
+});
 
-		console.log(`[DEBUG]: ALL TESTS PASSED\n`);
-	});
+// Noise.nearestCrest test
+then = Date.now();
+let output = Noise.nearestCrest(size, size, 0);
 
-	// Texture.generate test
-	then = Date.now();
-	Noise.Texture.generate().then((output) => {
-		let now = Date.now();
-		console.log(`[OUTPUT]: Texture.generate => ${typeof output}`);
-		console.log(`[DEBUG]: Texture.generate took ${(now - then) / 1000}s to execute`);
-		console.log(`[DEBUG]: Texture.generate -> TEST PASSED\n`);
-	});
+console.log(`[OUTPUT]: Noise.nearestCrest => [X:${output[0]}, Y:${output[1]}]`);
+console.log(`[DEBUG]: Noise.nearestCrest took ${(Date.now() - then) / 1000}s to execute`);
+console.log(`[DEBUG]: Noise.nearestCrest -> TEST PASSED\n`);
 
-	// Texture.ImageData test
-	then = Date.now();
-		let now = Date.now();
-		Noise.Texture.ImageData().then((output) => {
-		console.log(`[OUTPUT]: Texture.ImageData => ${now - then}ms`);
-		let png = new PNG({
-			width: output.width,
-			height: output.height,
-			inputHasAlpha: true
-		});
-		png.data = output.data;
-		png.pack()
-			.pipe(createWriteStream("pic.png"))
-			.on("finish", () => { console.log("[TEST] => All tests passed!") })
-	});
-} catch (e){
-	console.log(`[ERROR]: Worley has not installed correctly or an internal error has been raised.`);
-	console.log(`[REPO]: https://github.com/sokorototo/worley-noise`);
-	console.log(`[HELP]: In case of an internal error copy the following text and forward to the official repository: \n${e}\n`);
-}
+console.log(`[DEBUG]: ALL TESTS PASSED\n`);
+
+// Texture.generate test
+then = Date.now();
+output = Noise.Texture.generate();
+
+console.log(`[OUTPUT]: Texture.generate => ${typeof output}`);
+console.log(`[DEBUG]: Texture.generate took ${(Date.now() - then) / 1000}s to execute`);
+console.log(`[DEBUG]: Texture.generate -> TEST PASSED\n`);
+
+// Texture.ImageData test
+then = Date.now();
+output = Noise.Texture.ImageData();
+console.log(`[OUTPUT]: Texture.ImageData => ${Date.now() - then}ms`);
+
+// write PNG
+let png = new PNG({
+	width: output.width,
+	height: output.height,
+	inputHasAlpha: true
+});
+png.data = output.data;
+png.pack()
+	.pipe(createWriteStream("pic.png"))
+	.on("finish", () => { console.log("[TEST] => All tests passed!") })
