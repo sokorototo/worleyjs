fb63d930f7d397af31e93f3ca1137164af53aa73
Newton Toto <53893043+sokorototo@users.noreply.github.com>
2024-06-22 00:08:09 +0300

    Update worley.max.mjs

--- src/worley.max.mjs
+++ src/worley.max.mjs
@@ -1,6 +1,7 @@
 "use strict";
+
 class Worley {
-    constructor(setup = Worley.default()) {
+    constructor(setup = {}) {
         setup = Object.assign(Worley.default(), setup);
         // Texture parameters
         this.width = setup.width;
@@ -9,11 +10,12 @@ class Worley {
         // Algorithm parameters
         this.crests = [];
         this.threshold = setup.threshold;
-        if (this.threshold == undefined) {
+        if (this.threshold === undefined) {
             this.threshold = Worley.predictThreshold(setup.width, setup.height, setup.crests);
         };
-        Worley.generateCrests(setup, this);
-        this.hierachy = setup.hierachy;
+
+        this.generateCrests(setup);
+        this.hierarchy = setup.hierarchy;
         this.metric = setup.metric;
         this.interpolate = setup.interpolate;
         this.interpolant = setup.interpolant;
@@ -29,21 +31,19 @@ class Worley {
         this.Texture = {
             parent: this,
             generate() {
-                const data = new Uint8ClampedArray(this.parent.width * this.parent.height)
-                    , width = this.parent.width
-                    , height = this.parent.height;
+                const data = new Uint8Array(this.parent.width * this.parent.height);
+                const width = this.parent.width;
+                const height = this.parent.height;
                 for (let x = 0; x <= width; x++) {
                     for (let y = 0; y <= height; y++) {
-                        data[y * this.parent.width + x] = this.parent.pixel(x, y)
+                        data[y * this.parent.width + x] = this.parent.pixel(x, y);
                     }
                 };
-                this.data = data;
                 return data
             },
             data: null,
             ImageData(colors = this.parent.colors, alpha = this.parent.alpha, regenerate = false) {
-                if (!this.data || regenerate) this.generate();
-
+                let data = this.generate();
                 let imgData;
                 if (!(typeof window !== "undefined" && typeof window.ImageData === "function")) {
                     // Node or Deno environment
@@ -51,7 +51,7 @@ class Worley {
                         [Symbol.toStringTag]: "ImageData",
                         width: this.parent.width,
                         height: this.parent.height,
-                        data: new Uint8ClampedArray(this.parent.width * this.parent.height * 4)
+                        data: new Uint8Array(this.parent.width * this.parent.height * 4)
                     };
                 } else {
                     imgData = new ImageData(this.parent.width, this.parent.height)
@@ -60,10 +60,10 @@ class Worley {
                 // Populate the pixels
                 const rgbaChanelData = imgData.data.length;
                 for (let i = 0; i < rgbaChanelData; i++) {
-                    const scale = this.data[Math.floor(i / 4)] / 255;
+                    const scale = data[Math.floor(i / 4)] / 255;
 
                     // Alpha
-                    if ((i + 1) % 4 == 0) {
+                    if ((i + 1) % 4 === 0) {
                         // Number(false) returns 0
                         imgData.data[i] = 255 - (scale * 255 * Number(alpha));
                         continue;
@@ -71,7 +71,7 @@ class Worley {
 
                     // Monochrome
                     if (!colors) {
-                        imgData.data[i] = this.data[Math.floor(i / 4)];
+                        imgData.data[i] = data[Math.floor(i / 4)];
                         continue;
                     };
 
@@ -79,20 +79,22 @@ class Worley {
                     switch ((i + 1) % 4) {
                         case 1:
                             // Red Channel
-                            const minR = colors[0][0]
-                                , maxR = colors[1][0];
+                            const minR = colors[0][0];
+                            const maxR = colors[1][0];
+
                             imgData.data[i] = minR + ((maxR - minR) * scale);
                             break;
                         case 2:
                             // Green Channel
-                            const minG = colors[0][1]
-                                , maxG = colors[1][1];
+                            const minG = colors[0][1];
+                            const maxG = colors[1][1];
                             imgData.data[i] = minG + ((maxG - minG) * scale);
                             break;
                         case 3:
                             // Blue Channel
-                            const minB = colors[0][2]
-                                , maxB = colors[1][2];
+                            const minB = colors[0][2];
+                            const maxB = colors[1][2];
+
                             imgData.data[i] = minB + ((maxB - minB) * scale);
                             break;
                     }
@@ -105,30 +107,30 @@ class Worley {
 
     addCrest(x, y, relative = false) {
         // Cannot add undefined crest
-        if (x == undefined || y == undefined) throw new Error("Cannot add crest without given coordiantres.");
+        if (x == undefined || y == undefined) throw new Error("Cannot add crest without given coordinates.");
 
         // Check if co-ordinates are relative
         this.crests.push(relative ? [Math.round(x * this.width), Math.round(y * this.height)] : [x, y]);
     };
 
-    nearestCrest(x = 0, y = 0, hierachy = 0) {
-        if (this.crests.length === 0) return [Infinity, Infinity];
+    nearestCrest(x = 0, y = 0, hierarchy = 0) {
+        if (this.crests.length === 0) return null;
 
         // Manual for-loops are faster than higher order functions but array.sort is always an exception
-        const crests = this.crests.slice(0)
-            , iterations = crests.length;
+        const crests = this.crests.slice(0);
+        const iterations = crests.length;
         for (let i = 0; i < iterations; i++) {
             let crest = crests[i];
             crests[i] = { crest, distance: Worley.magnitude([x - crest[0], y - crest[1]]) }
         };
 
         // Find nearest crest
-        return Worley.find(crests, hierachy).crest;
+        return Worley.find(crests, hierarchy).crest;
     };
 
-    pixel(x, y, interpolate = this.interpolate, hierachy = this.hierachy) {
+    pixel(x, y, interpolate = this.interpolate, hierarchy = this.hierarchy) {
         x++;
-        let nearestCrest = this.nearestCrest(x, y, hierachy);
+        let nearestCrest = this.nearestCrest(x, y, hierarchy);
         let distance = Worley.magnitude([x - nearestCrest[0], y - nearestCrest[1]], this.metric);
         return interpolate ?
             this.interpolant(0, 255, Worley.clamp(distance / this.threshold, 0, 1)) :
@@ -151,31 +153,30 @@ class Worley {
         return (min + ((max - min) * x))
     };
 
-    static generateCrests(setup = Worley.default(), noise) {
+    generateCrests(setup) {
         // Initialize the Random Number Generator
         const rand = Worley.rand(
             setup.seed[0],
             setup.seed[1],
             setup.seed[2],
             setup.seed[3]
-        )
-            , prerun = setup.prerun
-            , crests = setup.crests;
+        );
+        const prerun = setup.prerun;
+        const crests = setup.crests;
         for (let i = 0; i <= prerun; i++) rand();
 
         // Generate the crests
-        for (let i = 0; i < crests; i++) noise.addCrest(rand(), rand(), true);
+        for (let i = 0; i < crests; i++) this.addCrest(rand(), rand(), true);
     };
 
     static magnitude(vector, metric = { type: "minkowski", p: 2 }) {
         switch (metric.type) {
-            case "euclidean":
-                return Math.sqrt((vector[0] ** 2 + vector[1] ** 2));
             case "manhattan":
                 return Math.abs(vector[0]) + Math.abs(vector[1]);
             case "minkowski":
                 return Math.pow((vector[0] ** metric.p + vector[1] ** metric.p), 1 / metric.p);
             default:
+                // euclidean
                 return Math.sqrt((vector[0] ** 2 + vector[1] ** 2));
         }
     };
@@ -189,7 +190,7 @@ class Worley {
             width: 100,
             height: 100,
             crests: 20,
-            hierachy: 0,
+            hierarchy: 0,
             interpolate: true,
             interpolant: Worley.interpolate,
             colors: false,
